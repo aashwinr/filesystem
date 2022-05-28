0b17ffe27b59d8079a158aa013bb31f10f829c85
Ashwin Rohit <aalagiri@ucsd.edu>
2022-05-27 17:27:24 -0700

    Filesystem: Add add file and directory methods

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,11 @@ use std::collections::HashMap;
 
 const CHUNK_COUNT: usize = 32;
 
+enum FileType {
+    File,
+    Directory,
+}
+
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct StorageChunk {
@@ -15,45 +20,74 @@ pub struct Filesystem {
 }
 
 pub struct Metadata {
-    files: HashMap<u32, File>,
+    files: HashMap<usize, File>,
 }
 
 impl Metadata {
     fn new() -> Self {
-        Self { files: HashMap::new() }
+        Self {
+            files: HashMap::new(),
+        }
     }
 }
 
 pub struct File {
     name: String,
-    id: u32,
-    chunk_ids: Vec<u32>,
+    id: usize,
+    parent_id: Option<usize>,
+    file_type: FileType,
+    chunk_ids: Vec<usize>,
+    subfiles: Option<Vec<usize>>,
 }
 
 impl File {
-    fn new(name: String, id: u32) -> Self {
+    fn new_file(name: String, id: usize, parent_id: Option<usize>) -> Self {
         Self {
             name,
             id,
+            parent_id,
+            file_type: FileType::File,
             chunk_ids: Vec::new(),
+            subfiles: None,
         }
     }
 
-    fn assign_chunk(&mut self, chunk_id: u32) {
+    fn new_directory(name: String, id: usize, parent_id: Option<usize>) -> Self {
+        Self {
+            name,
+            id,
+            parent_id,
+            file_type: FileType::Directory,
+            chunk_ids: Vec::new(),
+            subfiles: Some(Vec::new()),
+        }
+    }
+
+    fn assign_chunk(&mut self, chunk_id: usize) {
         self.chunk_ids.push(chunk_id);
     }
 }
 
 impl Filesystem {
     fn new() -> Self {
-        Self {
+        let mut ret = Self {
             id_ctr: 0,
             metadata: Metadata::new(),
-            storage: [StorageChunk {occupied: false} ;CHUNK_COUNT],
-        }
+            storage: [StorageChunk { occupied: false }; CHUNK_COUNT],
+        };
+        ret.add_directory(String::from("root"), None);
+        ret
     }
 
-    fn add_file(&mut self, name: String) {
-        
+    fn add_file(&mut self, name: String, parent_id: Option<usize>) {
+        let newfile = File::new_file(name, self.id_ctr, parent_id);
+        self.metadata.files.insert(self.id_ctr, newfile);
+        self.id_ctr += 1;
+    }
+
+    fn add_directory(&mut self, name: String, parent_id: Option<usize>) {
+        let newdirectory = File::new_directory(name, self.id_ctr, parent_id);
+        self.metadata.files.insert(self.id_ctr, newdirectory);
+        self.id_ctr += 1;
     }
 }
